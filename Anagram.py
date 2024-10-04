baeaea5ca60249e196d8c212dd6506b7193b4818
Amruth P S <100010797+co-de-er123@users.noreply.github.com>
2024-10-04 15:22:21 +0530

    Create Anagram.py

---
+++ Anagram.py
@@ -0,0 +1,11 @@
+str1 = "Listen"
+str2 = "Silent"
+
+str1 = list(str1.upper())
+str2 = list(str2.upper())
+str1.sort(), str2.sort()
+
+if(str1 == str2):
+    print("True")
+else:
+    print("False")
